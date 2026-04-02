fa1403c627dba51a6df70c45533e32a300930c1b
lKrematoryl <velychkova@gmail.com>
2026-04-02 20:18:55 +0700

    test fix and update

--- tests/end_to_end.py
+++ tests/end_to_end.py
@@ -16,16 +16,16 @@ class TestEndToEnd(HomePageNotLoggedIn):
             pass
 
     async def test_register_user(self, delete_account_after_test):
-        username = self.faker.user_name()
+        user = self.data.valid_user
         await self.home_page.header.signup_login_link.click(force=True)
-        await self.login_page.signup(username=username, email=self.faker.email())
-        await self.account_information_page.register_user(data=self.data.valid_user)
+        await self.login_page.signup(username=user.username, email=user.email)
+        await self.account_information_page.register_user(data=user)
 
         await expect(self.account_created_page.account_created.heading,
-                     message="Account Created heading should be displayed after registration").to_be_visible()
+                     "Account Created heading should be displayed after registration").to_be_visible()
         await self.account_created_page.continue_to_home()
 
         await expect(self.home_page.header.logged_in_as,
-                     message="User should be logged in after account creation").to_be_visible()
+                     "User should be logged in after account creation").to_be_visible()
         await expect(self.home_page.header.logged_in_as,
-                     message=f"Logged in username should be '{username}'").to_contain_text(username)
+                     f"Logged in username should be '{user.username}'").to_contain_text(user.username)
